6a4a0ed60666b7ab665477eb474a871a7249213d
a-bjn <andreibejan55@yahoo.com>
2024-08-31 03:28:39 +0300

    add more styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,29 @@
 import '../styles/Header.css'
+import '../styles/App.css'
 
 const Header = () => {
     return (
         <>
         <div className="header-content-wrap">
             <div className="logo-wrap">
-                <p className="big-text">Reparatii Braila</p>
-                <p className="small-text">TV/LCD/LED... CD/DVD AUTO... TELECOMENZI</p>
+                <p className="big-text thicker-text">Reparatii TV Braila</p>
+                <p className="small-text thin-text">TV/LCD/LED... CD/DVD AUTO... TELECOMENZI</p>
             </div>
             <div className="information-wrap">
                 <div className="information-component">
-                <p className="medium-text">Contact:</p>
+                <p className="medium-text thicker-text">Contact:</p>
                     <img src="src/assets/telefon.svg"/>
-                <p className="medium-text text-underline">+40 0735156317</p>
+                <p className="medium-text text-underline thin-text">+40 0735156317</p>
                 </div>
                 <div className="information-component">
-                <p className="medium-text">Locatie:</p>
+                <p className="medium-text thicker-text">Locatie:</p>
                     <img src="src/assets/pinpoint.svg"></img>
-                <p className="medium-text text-underline">Str. General Eremia Grigorescu 40</p>
+                <p className="medium-text text-underline thin-text">Str. General Eremia Grigorescu 40</p>
                 </div>
                 <div className="information-component">
-                <p className="medium-text">Program:</p>
+                <p className="medium-text thicker-text">Program:</p>
                     <img src="src/assets/calendar.svg"></img>
-                <p className="medium-text text-underline">Luni / Vineri 8:00-17:00</p>
+                <p className="medium-text text-underline thin-text">Luni / Vineri 8:00-17:00</p>
                 </div>
             </div>
         </div>
